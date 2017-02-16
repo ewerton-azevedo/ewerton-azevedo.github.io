76b108280d187e974858276eb3900347ac68f1dc
Ewerton Azevedo <ewerton.azevedo@live.co.uk>
2017-02-16 11:42:15 +0000

    import jq original library

--- jq-animate-library.js
+++ jq-animate-library.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function(){
  var fadeIn = function(X){
- var x = '$(".fadeIn")';
-  x.css("display", "none"); 
-  x.fadeIn(3000);
+ 
+  $('.fadeIn').css("display", "none"); 
+  $('.fadeIn').fadeIn(3000);
   
 }
 fadeIn();})
